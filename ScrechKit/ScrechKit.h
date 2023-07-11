eb16e8837fdafb38a5ede31dff4d8667b6e90aba
Sergei Saliukov <sergei_saliukov@icloud.com>
2023-07-11 13:34:21 +0200

    Initial Commit

---
+++ ScrechKit/ScrechKit.h
@@ -0,0 +1,18 @@
+//
+//  ScrechKit.h
+//  ScrechKit
+//
+//  Created by Sergei Saliukov on 11/07/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ScrechKit.
+FOUNDATION_EXPORT double ScrechKitVersionNumber;
+
+//! Project version string for ScrechKit.
+FOUNDATION_EXPORT const unsigned char ScrechKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ScrechKit/PublicHeader.h>
+
+
