3c0eeba58972fc191478d43c1c684a1da36d530f
Sergei Saliukov <sergei_saliukov@icloud.com>
2023-10-15 18:42:30 +0200

    -

--- ScrechKit.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-
-//! Project version number for ScrechKit.
-FOUNDATION_EXPORT double ScrechKitVersionNumber;
-
-//! Project version string for ScrechKit.
-FOUNDATION_EXPORT const unsigned char ScrechKitVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <ScrechKit/PublicHeader.h>
