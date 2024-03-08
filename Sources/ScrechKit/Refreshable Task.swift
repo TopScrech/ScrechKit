e0212525cf9d72ea2ae6b8631e6ea070348f010a
TopScrech <sergei_saliukov@icloud.com>
2024-03-08 10:11:41 +0100

    Fix

--- Sources/ScrechKit/Refreshable Task.swift
+++ Sources/ScrechKit/Refreshable Task.swift
@@ -2,7 +2,7 @@ import SwiftUI
 
 @available(iOS 15, macOS 12, tvOS 15, watchOS 8, *)
 public extension View {
-    func refreshableTask <ID> (_ action: @escaping () -> Void) -> some View {
+    func refreshableTask(_ action: @escaping () -> Void) -> some View {
         self
             .task {
                 action()
