505b04d9b4c4963919308cb61c1ecbac0fc9d036
TopScrech <sergei_saliukov@icloud.com>
2024-04-06 10:42:29 +0200

    Fix

--- Sources/ScrechKit/Notifications/UN Notification Sound.swift
+++ Sources/ScrechKit/Notifications/UN Notification Sound.swift
@@ -1,15 +1,19 @@
 #if !os(tvOS)
 
-@available(iOS 10, macOS 10.14, watchOS 3, *)
+import UserNotifications
+
 public extension UNNotificationSound {
+    @available(iOS 15.2, *)
     static func ringtone(_ name: String) -> UNNotificationSound {
         .ringtoneSoundNamed(.init(name))
     }
     
+    @available(iOS 12, macOS 10.14, *)
     static func critical(_ name: String) -> UNNotificationSound {
         .criticalSoundNamed(.init(name))
     }
     
+    @available(iOS 12, macOS 10.14, *)
     static func critical(_ name: String, volume: Float) -> UNNotificationSound {
         .criticalSoundNamed(.init(name), withAudioVolume: volume)
     }
