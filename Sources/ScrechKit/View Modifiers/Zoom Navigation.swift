42adace45be82a1c46618d36b8c9e5f521b19851
TopScrech <sergei_saliukov@icloud.com>
2025-02-10 02:20:57 +0100

    Zoom nav transition

---
+++ Sources/ScrechKit/View Modifiers/Zoom Navigation.swift
@@ -0,0 +1,52 @@
+import SwiftUI
+
+@available(iOS 17, macOS 14, tvOS 17, watchOS 10, *)
+public struct NavigationZoomTransition: ViewModifier {
+    private let sourceID: String
+    private let namespace: Namespace.ID
+    
+    init(_ sourceID: String, namespace: Namespace.ID) {
+        self.sourceID = sourceID
+        self.namespace = namespace
+    }
+    
+    public func body(content: Content) -> some View {
+        if #available(iOS 18, *) {
+            content
+                .navigationTransition(.zoom(sourceID: sourceID, in: namespace))
+        } else {
+            content
+        }
+    }
+}
+
+@available(iOS 17, macOS 14, tvOS 17, watchOS 10, *)
+public struct MatchedTransitionSource: ViewModifier {
+    private let id: String
+    private let namespace: Namespace.ID
+    
+    init(_ id: String, namespace: Namespace.ID) {
+        self.id = id
+        self.namespace = namespace
+    }
+    
+    public func body(content: Content) -> some View {
+        if #available(iOS 18.0, *) {
+            content
+                .matchedTransitionSource(id: id, in: namespace)
+        } else {
+            content
+        }
+    }
+}
+
+@available(iOS 17, macOS 14, tvOS 17, watchOS 10, *)
+public extension View {
+    func navigationZoomTransition(_ sourceID: String, in namespace: Namespace.ID) -> some View {
+        modifier(NavigationZoomTransition(sourceID, namespace: namespace))
+    }
+    
+    func matchedTransitionSource(_ id: String, in namespace: Namespace.ID) -> some View {
+        modifier(MatchedTransitionSource(id, namespace: namespace))
+    }
+}
