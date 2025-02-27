25bb79b2e34686515629a7e4f582244cbda5dea1
TopScrech <sergei_saliukov@icloud.com>
2025-02-27 12:30:23 +0100

    .popover

--- Sources/ScrechKit/View Modifiers/Sheet.swift
+++ Sources/ScrechKit/View Modifiers/Sheet.swift
@@ -2,7 +2,17 @@ import SwiftUI
 
 @available(iOS 13, macOS 10.15, tvOS 13, watchOS 6, *)
 public extension View {
-    func sheet <Content> (
+    func sheet<Content> (
+        _ isPresented: Binding<Bool> = .constant(true),
+        @ViewBuilder content: @escaping () -> Content
+    ) -> some View where Content: View {
+        self.sheet(isPresented: isPresented, content: content)
+    }
+}
+
+@available(iOS 13, macOS 10.15, tvOS 13, watchOS 6, *)
+public extension View {
+    func popover<Content> (
         _ isPresented: Binding<Bool> = .constant(true),
         @ViewBuilder content: @escaping () -> Content
     ) -> some View where Content: View {
@@ -14,7 +24,7 @@ public extension View {
 
 @available(iOS 17, macOS 14, *)
 public extension View {
-    func inspector <Content> (
+    func inspector<Content> (
         _ isPresented: Binding<Bool> = .constant(true),
         @ViewBuilder content: @escaping () -> Content
     ) -> some View where Content: View {
