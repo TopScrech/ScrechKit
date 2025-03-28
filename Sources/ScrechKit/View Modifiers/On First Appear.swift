7269c00f5e353c581924456dd4ae84a0424e41f3
TopScrech <sergei_saliukov@icloud.com>
2025-03-29 00:54:16 +0100

    On first appear modifier now supports async

--- Sources/ScrechKit/View Modifiers/On First Appear.swift
+++ Sources/ScrechKit/View Modifiers/On First Appear.swift
@@ -2,14 +2,20 @@ import SwiftUI
 
 @available(iOS 13, macOS 10.15, tvOS 13, watchOS 6, *)
 public extension View {
-    func onFirstAppear(_ action: @escaping () -> ()) -> some View {
+    /// Runs the synchronous action the first time the view appears
+    func onFirstAppear(_ action: @escaping () -> Void) -> some View {
         modifier(FirstAppear(action))
+    }
+    
+    /// Runs the asynchronous action the first time the view appears
+    func onFirstAppear(_ action: @escaping () async -> Void) -> some View {
+        modifier(FirstAppearAsync(action))
     }
 }
 
 @available(iOS 13, macOS 10.15, tvOS 13, watchOS 6, *)
 private struct FirstAppear: ViewModifier {
-    private let action: () -> ()
+    private let action: () -> Void
     
     init(_ action: @escaping () -> Void) {
         self.action = action
@@ -25,6 +31,31 @@ private struct FirstAppear: ViewModifier {
             
             hasAppeared = true
             action()
+        }
+    }
+}
+
+@available(iOS 13, macOS 10.15, tvOS 13, watchOS 6, *)
+private struct FirstAppearAsync: ViewModifier {
+    private let action: () async -> Void
+    
+    init(_ action: @escaping () async -> Void) {
+        self.action = action
+    }
+    
+    @State private var hasAppeared = false
+    
+    func body(content: Content) -> some View {
+        content.onAppear {
+            guard !hasAppeared else {
+                return
+            }
+            
+            hasAppeared = true
+            
+            Task {
+                await action()
+            }
         }
     }
 }
