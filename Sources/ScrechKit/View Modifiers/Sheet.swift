c2c671d9d59bf6f6cb2d9ff04838d1bc0e142719
TopScrech <sergei_saliukov@icloud.com>
2025-03-29 00:21:32 +0100

    fullScreenCover

--- Sources/ScrechKit/View Modifiers/Sheet.swift
+++ Sources/ScrechKit/View Modifiers/Sheet.swift
@@ -20,8 +20,19 @@ public extension View {
     }
 }
 
-#if os(macOS) || os(iOS) && !os(visionOS)
+#if !os(macOS)
+@available(iOS 14, tvOS 14, watchOS 7, *)
+public extension View {
+    func fullScreenCover<Content> (
+        _ isPresented: Binding<Bool> = .constant(true),
+        @ViewBuilder content: @escaping () -> Content
+    ) -> some View where Content: View {
+        self.fullScreenCover(isPresented: isPresented, content: content)
+    }
+}
+#endif
 
+#if os(macOS) || os(iOS) && !os(visionOS)
 @available(iOS 17, macOS 14, *)
 public extension View {
     func inspector<Content> (
@@ -31,5 +42,4 @@ public extension View {
         self.inspector(isPresented: isPresented, content: content)
     }
 }
-
 #endif
