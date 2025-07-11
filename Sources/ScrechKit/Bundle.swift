ab825871ce1732681425385c9288817379c3f824
TopScrech <sergei_saliukov@icloud.com>
2025-07-11 11:37:07 +0200

    added Bundle.build

--- Sources/ScrechKit/Bundle.swift
+++ Sources/ScrechKit/Bundle.swift
@@ -4,4 +4,8 @@ public extension Bundle {
     static var version: String? {
         self.main.infoDictionary?["CFBundleShortVersionString"] as? String
     }
+    
+    static var build: String? {
+        self.main.infoDictionary?["CFBundleVersion"] as? String
+    }
 }
