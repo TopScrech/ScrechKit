d39ec2209d1e7b72024a4f11cb97fcee4e0e873c
TopScrech <sergei_saliukov@icloud.com>
2025-08-07 00:31:04 +0300

    pasteboard fix

--- Sources/ScrechKit/File.swift
+++ Sources/ScrechKit/File.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-final class Pasteboard {
+public final class Pasteboard {
     static func copy(_ string: String) {
 #if os(macOS)
         NSPasteboard.general.setString(string, forType: .string)
