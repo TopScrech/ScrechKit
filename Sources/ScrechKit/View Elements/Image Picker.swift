a5b6a20aa3c512ec6791c0b8aaf8cb09493e6810
TopScrech <sergei_saliukov@icloud.com>
2025-11-10 23:01:28 +0100

    -

--- Sources/ScrechKit/View Elements/Image Picker.swift
+++ Sources/ScrechKit/View Elements/Image Picker.swift
@@ -46,7 +46,9 @@ public struct ImagePicker: UIViewControllerRepresentable {
         public func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
             picker.dismiss(animated: true)
             
-            guard let provider = results.first?.itemProvider else { return }
+            guard let provider = results.first?.itemProvider else {
+                return
+            }
             
             if provider.canLoadObject(ofClass: UIImage.self) {
                 provider.loadObject(ofClass: UIImage.self) { image, _ in
