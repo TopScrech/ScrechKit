3849439c0f9ca858d7ae6a90c2ca85fadbe33da9
TopScrech <sergei_saliukov@icloud.com>
2025-11-20 12:14:45 +0100

    fix

--- Sources/ScrechKit/View Elements/LabeledContent.swift
+++ Sources/ScrechKit/View Elements/LabeledContent.swift
@@ -1,7 +1,7 @@
 import SwiftUI
 
 @available(iOS 16, macOS 13, tvOS 16, watchOS 9, *)
-extension LabeledContent where Label == SwiftUI.Label<Text, Image>, Content == Text {
+public extension LabeledContent where Label == SwiftUI.Label<Text, Image>, Content == Text {
     init(_ title: String, icon: String, value: String) {
         self.init {
             Text(value)
