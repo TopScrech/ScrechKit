68dc17cb9990705573864bc039b88f27ad317c25
TopScrech <sergei_saliukov@icloud.com>
2025-12-06 22:23:06 +0100

    added timeSinceISO(_ date: Date)

--- Sources/ScrechKit/timeSinceISO.swift
+++ Sources/ScrechKit/timeSinceISO.swift
@@ -31,3 +31,29 @@ public func timeSinceISO(_ date: String) -> LocalizedStringKey {
     let sinceNowDays = sinceNowHours / 24
     return "\(sinceNowDays) days ago"
 }
+
+@available(iOS 16, macOS 13, tvOS 16, watchOS 9, *)
+public func timeSinceISO(_ date: Date) -> LocalizedStringKey {
+    let sinceNowSeconds = Int(date.timeIntervalSinceNow * -1)
+    
+    guard sinceNowSeconds > 1 else {
+        return "Now"
+    }
+    
+    guard sinceNowSeconds > 60 else {
+        return "\(sinceNowSeconds) seconds ago"
+    }
+    
+    let sinceNowMinutes = sinceNowSeconds / 60
+    guard sinceNowMinutes > 60 else {
+        return "\(sinceNowMinutes) minutes ago"
+    }
+    
+    let sinceNowHours = sinceNowMinutes / 60
+    guard sinceNowHours > 24 else {
+        return "\(sinceNowHours) hours ago"
+    }
+    
+    let sinceNowDays = sinceNowHours / 24
+    return "\(sinceNowDays) days ago"
+}
