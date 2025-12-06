5a13f3a33a31143db30352f93bac207fb4634699
TopScrech <sergei_saliukov@icloud.com>
2025-12-06 22:47:21 +0100

    formatISO(_ date: Date)

--- Sources/ScrechKit/formatISO.swift
+++ Sources/ScrechKit/formatISO.swift
@@ -13,3 +13,12 @@ public func formatISO(_ date: String) -> String {
     
     return formatter.string(from: date)
 }
+
+public func formatISO(_ date: Date) -> String {
+    let formatter = DateFormatter()
+    formatter.dateFormat = "dd-MM-yyyy"
+    formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+    formatter.locale = Locale(identifier: "en_US_POSIX")
+    
+    return formatter.string(from: date)
+}
