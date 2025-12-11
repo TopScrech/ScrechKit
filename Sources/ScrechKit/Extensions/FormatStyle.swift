92974e8ef8592665054bf68ae6adfa31cadfb218
TopScrech <sergei_saliukov@icloud.com>
2025-12-11 20:33:35 +0100

    added FormatStylefractionDigits

---
+++ Sources/ScrechKit/Extensions/FormatStyle.swift
@@ -0,0 +1,17 @@
+@available(iOS 15, macOS 12, tvOS 15, watchOS 8, *)
+public extension FormatStyle where Self == FloatingPointFormatStyle<Double>.Percent {
+    static var percentRounded: Self {
+        .percent.precision(.fractionLength(0))
+    }
+}
+
+@available(iOS 15, macOS 12, tvOS 15, watchOS 8, *)
+public extension FormatStyle where Self == FloatingPointFormatStyle<Double> {
+    static func fractionDigits(_ digits: Int) -> Self {
+        .number.precision(.fractionLength(digits))
+    }
+    
+    static func fractionDigits(_ range: ClosedRange<Int>) -> Self {
+        .number.precision(.fractionLength(range))
+    }
+}
--- Sources/ScrechKit/Extensions/percentRounded.swift
+++
@@ -1,6 +0,0 @@
-@available(iOS 15, macOS 12, tvOS 15, watchOS 8, *)
-public extension FormatStyle where Self == FloatingPointFormatStyle<Double>.Percent {
-    static var percentRounded: Self {
-        .percent.precision(.fractionLength(0))
-    }
-}
