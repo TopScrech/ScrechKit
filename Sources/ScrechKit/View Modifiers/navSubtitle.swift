482651e4f2f83a1b55cf7d70372d00cf5a445e04
TopScrech <sergei_saliukov@icloud.com>
2026-01-30 04:46:00 +0100

    added navSubtitle

---
+++ Sources/ScrechKit/View Modifiers/navSubtitle.swift
@@ -0,0 +1,22 @@
+import SwiftUI
+
+@available(iOS 13, macOS 10.15, tvOS 13, watchOS 6, *)
+public extension View {
+    @ViewBuilder
+    func navSubtitle(_ subtitle: LocalizedStringKey) -> some View {
+#if os(visionOS) || os(tvOS)
+        self
+#else
+        if #available(iOS 26, macOS 11, *) {
+            self
+                .navigationSubtitle(subtitle)
+        } else {
+            self
+        }
+#endif
+    }
+    
+    func navSubtitle<T: CustomStringConvertible>(_ subtitle: T) -> some View {
+        navSubtitle(LocalizedStringKey(subtitle.description))
+    }
+}
