fd773c46059dfaa41dbe3e754ba672f1273ea074
TopScrech <sergei_saliukov@icloud.com>
2026-03-19 21:25:00 +0100

    -

--- Sources/ScrechKit/Functions/openSettings.swift
+++ Sources/ScrechKit/Functions/openSettings.swift
@@ -4,7 +4,7 @@ import Foundation
 @MainActor
 public func openSettings(_ onCompletion: @escaping () -> Void = {}, onFailure: @escaping () -> Void = {}) {
     if let url = URL(string: UIApplication.openSettingsURLString) {
-        UIApplication.shared.open(url, options: [:]) {_ in
+        UIApplication.shared.open(url, options: [:]) { _ in
             onCompletion()
         }
     } else {
