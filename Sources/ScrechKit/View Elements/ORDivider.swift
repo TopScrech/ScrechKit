fa0df6751173daac9349e6f8c01bb8f9d01cb112
TopScrech <sergei_saliukov@icloud.com>
2026-05-27 16:16:29 +0200

    Added a public init for ORDivider

--- Sources/ScrechKit/View Elements/ORDivider.swift
+++ Sources/ScrechKit/View Elements/ORDivider.swift
@@ -2,6 +2,8 @@ import SwiftUI
 
 @available(iOS 13, macOS 10.15, tvOS 13, watchOS 6, *)
 public struct ORDivider: View {
+    public init() {}
+    
     public var body: some View {
         HStack {
             VStack {
