c8ba270efdd30dd43f1e178c7ddc79b57474503c
TopScrech <sergei_saliukov@icloud.com>
2026-05-27 12:51:00 +0200

    Added ORDivider

---
+++ Sources/ScrechKit/View Elements/ORDivider.swift
@@ -0,0 +1,20 @@
+import SwiftUI
+
+@available(iOS 13, macOS 10.15, tvOS 13, watchOS 6, *)
+public struct ORDivider: View {
+    public var body: some View {
+        HStack {
+            VStack {
+                Divider()
+            }
+            
+            Text("or")
+                .secondary()
+            
+            VStack {
+                Divider()
+            }
+        }
+        .padding(8)
+    }
+}
