cc47ee7d271fd85f5e95afdb83e3d45edadf2327
TopScrech <89252798+TopScrech@users.noreply.github.com>
2026-09-22 23:46:21 +0200

    AsyncButton is now a Button wrapper that simply performs an async task when tapped

--- Sources/ScrechKit/AsyncButton.swift
+++ Sources/ScrechKit/AsyncButton.swift
@@ -1,72 +1,52 @@
 import SwiftUI
 
-@available(iOS 17, macOS 14, tvOS 17, watchOS 10, *)
 /// A button that performs an async task when tapped
-public struct AsyncButton<Label: View, Trigger: Equatable>: View {
-    /// The cancellation trigger
-    var cancellation: Trigger?
+public struct AsyncButton<Label: View>: View {
+    private let role: ButtonRole?
+    private let action: () async -> Void
+    private let label: () -> Label
     
-    /// The asynchronous action to perform when the button is tapped
-    var action: () async throws -> Void
-    
-    /// The label of the button
-    @ViewBuilder var label: () -> Label
-    
-    /// Whether the task is currently being performed
-    @State private var isPerformingTask = false
-    
-    /// The error message to display if the task fails
-    @State private var errorMessage: String?
-    
-    /// Whether to show the alert
-    @State private var showAlert = false
-    
-    /// The underlying task for cancellation
-    @State private var task: Task<Void, Never>?
-    
-    init(cancellation: Trigger? = nil, action: @escaping () async throws -> Void, @ViewBuilder label: @escaping () -> Label) {
-        self.cancellation = cancellation
+    public init(role: ButtonRole? = nil, action: @escaping () async -> Void, @ViewBuilder label: @escaping () -> Label) {
+        self.role = role
         self.action = action
         self.label = label
     }
     
     public var body: some View {
-        Button {
-            isPerformingTask = true
-            
-            task = Task {
-                do {
-                    try await action()
-                    errorMessage = nil
-                    
-                } catch is CancellationError {
-                    errorMessage = nil
-                    
-                } catch {
-                    errorMessage = error.localizedDescription
-                }
-                
-                isPerformingTask = false
-                showAlert = errorMessage != nil
+        Button(role: role) {
+            Task {
+                await action()
             }
         } label: {
-            HStack {
-                if isPerformingTask {
-                    ProgressView()
-                        .controlSize(.mini)
-                }
-                
-                label()
-            }
+            label()
         }
-        .disabled(isPerformingTask)
-        .onChange(of: cancellation) {
-            task?.cancel()
+    }
+}
+
+public extension AsyncButton where Label == Text {
+    init(_ titleKey: LocalizedStringKey, role: ButtonRole? = nil, action: @escaping () async -> Void) {
+        self.init(role: role, action: action) {
+            Text(titleKey)
         }
-        .alert("Error", isPresented: $showAlert) {
-            Button("OK") {}
-        } message: {
-            Text(errorMessage ?? "")
+    }
+    
+    init<S: StringProtocol>(_ title: S, role: ButtonRole? = nil, action: @escaping () async -> Void) {
+        self.init(role: role, action: action) {
+            Text(title)
+        }
+    }
+}
+
+public extension AsyncButton where Label == SwiftUI.Label<Text, Image> {
+    init(_ titleKey: LocalizedStringKey, systemImage: String, role: ButtonRole? = nil, action: @escaping () async -> Void) {
+        self.init(role: role, action: action) {
+            SwiftUI.Label(titleKey, systemImage: systemImage)
+        }
+    }
+    
+    init<S: StringProtocol>(_ title: S, systemImage: String, role: ButtonRole? = nil, action: @escaping () async -> Void) {
+        self.init(role: role, action: action) {
+            SwiftUI.Label(title, systemImage: systemImage)
         }
     }
 }
